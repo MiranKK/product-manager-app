714d32bed0b826036948083e71bdf325fe19fadc
MiranKK <mirankawa2002@gmail.com>
2026-03-25 08:52:55 +0300

    Add register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,10 +1,223 @@
+import 'package:clothes_store_app/style/custom_text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _key = GlobalKey<FormState>();
+
+  bool isValidPassword(String password) {
+    final passwordRegex = RegExp(
+      r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+    );
+
+    return passwordRegex.hasMatch(password);
+  }
+
+  bool isValidEmail(String email) {
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+    return emailRegex.hasMatch(email);
+  }
+
+  bool _isvisable = true;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(10.r),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  "Register",
+                  style: CustomTextStyle().largeBlackBoldText,
+                ),
+              ),
+              SizedBox(height: 15.h),
+              Form(
+                key: _key,
+                child: Column(
+                  spacing: 15.h,
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Username is required";
+                        }
+                        return null;
+                      },
+                      style: CustomTextStyle().xxsmallBlackText,
+                      decoration: InputDecoration(
+                        errorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.h),
+                        prefixIcon: Icon(Icons.person_2_outlined, size: 18.w),
+                        hintText: "Enter Username",
+                        hintStyle: CustomTextStyle().xxsmallBlackText.copyWith(
+                          color: Colors.grey,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey.shade200,
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Email is required";
+                        }
+                        if (!isValidEmail(value)) {
+                          return 'Enter a valid email address';
+                        }
+                        return null;
+                      },
+                      style: CustomTextStyle().xxsmallBlackText,
+                      decoration: InputDecoration(
+                        errorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.h),
+                        prefixIcon: Icon(Icons.email_outlined, size: 18.w),
+                        hintText: "Enter Email",
+                        hintStyle: CustomTextStyle().xxsmallBlackText.copyWith(
+                          color: Colors.grey,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey.shade200,
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Password is required";
+                        }
+                        if (value.length < 8) {
+                          return "Password must more than 7 character";
+                        }
+                        if (!isValidPassword(value)) {
+                          return 'Password must contain letter, number & special character';
+                        }
+
+                        return null;
+                      },
+                      obscureText: _isvisable,
+
+                      style: CustomTextStyle().xxsmallBlackText,
+                      decoration: InputDecoration(
+                        errorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.red),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.h),
+                        prefixIcon: Icon(Icons.lock_outline, size: 18.w),
+                        hintText: "Enter Password",
+                        hintStyle: CustomTextStyle().xxsmallBlackText.copyWith(
+                          color: Colors.grey,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey.shade200,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _isvisable = !_isvisable;
+                            });
+                          },
+                          child: _isvisable
+                              ? Icon(Icons.visibility_off, size: 18.w)
+                              : Icon(Icons.visibility, size: 18.w),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Already have an account?",
+                          style: CustomTextStyle().xxsmallBlackText.copyWith(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, "/login");
+                          },
+                          child: Text(
+                            "Login",
+                            style: CustomTextStyle().xxsmallBlackBoldText,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(280.w, 52.h),
+                        backgroundColor: const Color(0xff9775FA),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                      ),
+                      onPressed: () {
+                        _key.currentState?.validate();
+                      },
+                      child: Text(
+                        "Register",
+                        style: CustomTextStyle().xxsmallWhiteBoldText,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
